4e50e1f11b99faf4bf5c92de2633b82b7908756e
Arthur <Art1choke@yandex.com>
2022-03-25 00:03:56 +0300

    fix programm

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastMovieTitle = prompt('Один из последних просмотренных фильмов?', 'Logan');
-const lastMovieRating = +prompt('На сколько оцените его?', '8.1');
+let lastMovieTitle = prompt('Один из последних просмотренных фильмов?', 'Logan'),
+      lastMovieRating = +prompt('На сколько оцените его?', '8.1');
 
-const movies = {
-    lastMovieTitle: lastMovieRating
-};
+personalMovieDB['movies'][lastMovieTitle] = lastMovieRating;
 
-/* console.log(personalMovieDB.count);
-console.log(personalMovieDB.privat);
-console.log(movies);
-console.log(movies['lastMovieTitle']); */
+lastMovieTitle = prompt('Один из последних просмотренных фильмов?', 'No Way Home');
+lastMovieRating = +prompt('На сколько оцените его?', '7.9');
+
+personalMovieDB['movies'][lastMovieTitle] = lastMovieRating;
+
+// console.log(personalMovieDB.movies);
