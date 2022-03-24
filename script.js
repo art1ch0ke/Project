cd66c4de5bc781fb5b6614d1972dbd9806cee657
Arthur <Art1choke@yandex.com>
2022-03-24 23:43:51 +0300

    fix mistake in prompt command, wrapped test in comment

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastMovieTitle = promt('Один из последних просмотренных фильмов?', 'Logan');
-const lastMovieRating = +promt('На сколько оцените его?', '8.1');
+const lastMovieTitle = prompt('Один из последних просмотренных фильмов?', 'Logan');
+const lastMovieRating = +prompt('На сколько оцените его?', '8.1');
 
 const movies = {
     lastMovieTitle: lastMovieRating
 };
 
-console.log(personalMovieDB.count);
+/* console.log(personalMovieDB.count);
 console.log(personalMovieDB.privat);
 console.log(movies);
-console.log(movies['lastMovieTitle']);
+console.log(movies['lastMovieTitle']); */
