65dd73255daa8342209a0ee16091578b70b77609
Arthur <Art1choke@yandex.com>
2022-03-24 23:29:19 +0300

    add variables lastMovieTitle and lastMovieRating, add object movies

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
+};
+
+const lastMovieTitle = promt('Один из последних просмотренных фильмов?', 'Logan');
+const lastMovieRating = +promt('На сколько оцените его?', '8.1');
+
+const movies = {
+    lastMovieTitle: lastMovieRating
 };
